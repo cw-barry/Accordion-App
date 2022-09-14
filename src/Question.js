119a9caf169766df7e8e821ef6612b77144b04c1
cw-barry <barry@clarusway.com>
2022-09-14 15:59:50 -0400

    handle questions

--- src/Question.js
+++ src/Question.js
@@ -1,15 +1,19 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 const Question = ({ title, info }) => {
+  const [displayDetails, setDisplayDetails] = useState(false);
   return (
     <article className="question">
       <header>
         <h4>{title}</h4>
-        <button className="btn">
-          <AiOutlinePlus />
+        <button
+          className="btn"
+          onClick={() => setDisplayDetails(!displayDetails)}
+        >
+          {displayDetails ? <AiOutlineMinus /> : <AiOutlinePlus />}
         </button>
       </header>
-      {/* put info here */}
+      {displayDetails && <p>{info}</p>}
     </article>
   );
 };
